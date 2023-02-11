f9c260f54fc4c0b1f45752148c8fb66410c70ef9
MCillo <117599499+MCillo@users.noreply.github.com>
2023-02-11 14:58:03 -0500

    Create scripts.js
    created javascript file,

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//Javascript from Exercise 1.1 Full Stack Imersion
+
+
+alert('Hello There');   // creates a popup message on page load that displays message 'Hello There'
+
+let favoriteFood = 'Steak'   // creates variable named favoriteFood and assigns it the value Steak
+document.write(favoriteFood);   //displays contents of variable named favoriteFood on page
+
