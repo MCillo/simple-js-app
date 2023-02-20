e00f6809e93dce715970972cc9716bbd4badaf5a
MCillo <117599499+MCillo@users.noreply.github.com>
2023-02-19 19:24:37 -0500

    Update scripts.js
    commented out the for loop and added a forEach() function to replace it,

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@ let pokemonList = [
     {name: 'Squirtle', height: '0.5', type: "water"},
 ];
 
-/* creates a for loop, initializing variable i at 0 for the start of the array, the condition is for the length or number of objects in the array, the action is to add 1 
- to the variable i and to continue until the number of objects in the array is met */
+/*creates a for loop, initializing variable i at 0 for the start of the array, the condition is for the length or number of objects in the array, the action is to add 1 
+ to the variable i and to continue until the number of objects in the array is met
 for (let i=0; i < pokemonList.length; i++){ 
     if (pokemonList[i].height < 0.6 && pokemonList[i].height > 0.4){ //if statement checks if height is less than 0.6 and greater than 0.4
         document.write(pokemonList[i].name + "\n weighs \n" + pokemonList[i].height + " that's an average weight." + "<br>"); // if above if statement is true then prints out pokemon name + text + pokemon weight + text, if statement is false then continues to next line
@@ -17,3 +17,8 @@ for (let i=0; i < pokemonList.length; i++){
         document.write(pokemonList[i].name + "\n weighs \n"  + pokemonList[i].height + " that is underweight." + "<br>"); // prints out pokemon name + text + pokemon weight + text
     }
 }
+*/
+// using a forEach() function to replace the for loop on lines 11 through 19 above
+pokemonList.forEach(function(pokemonList) {
+    document.write(pokemonList.name + "\n is \n"  + pokemonList.height + " meters tall." + "<br>");
+});
